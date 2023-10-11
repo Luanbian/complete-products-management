9aa9c5da04f1d727f4eb12a8e4dd6b769d71b278
luan <luanbianchine1@gmail.com>
2023-10-11 00:11:05 -0300

    feat: create migration with flyway

--- src/main/java/study/project/Productmanagement/ProductManagementApplication.java
+++ src/main/java/study/project/Productmanagement/ProductManagementApplication.java
@@ -1,13 +1,21 @@
 package study.project.Productmanagement;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProductManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductManagementApplication.class, args);
+	}
+	@Bean
+	public Flyway flyway() {
+		Flyway flyway = Flyway.configure().dataSource("jdbc:postgresql://localhost:5432/products", "luan", "02122001").load();
+		flyway.migrate();
+		return flyway;
 	}
 
 }
